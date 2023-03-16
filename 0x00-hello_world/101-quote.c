1c54151679199289ed39cd83b55a6c8797774b86
viictoo <provicml@gmail.com>
2023-03-16 11:03:47 +0000

    adv tested and cleaned

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,6 @@
 #include <unistd.h>
+#include <stdio.h>
+
 /*
  * main - A program that prints a string
  * Return: 1(exit status error)
@@ -6,6 +8,6 @@
 
 int main(void)
 {
-	fput("and that piece of art is useful\" \- Dora Korpar, 2015\-10\-19\n", stdout);
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
