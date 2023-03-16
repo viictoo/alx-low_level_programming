832424e7801bfeb00acbdc7123739676e01da373
viictoo <provicml@gmail.com>
2023-03-16 10:59:12 +0000

    adv tested

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+/*
+ * main - A program that prints a string
+ * Return: 1(exit status error)
+ */
+
+int main(void)
+{
+	fput("and that piece of art is useful\" \- Dora Korpar, 2015\-10\-19\n", stdout);
+	return (1);
+}
