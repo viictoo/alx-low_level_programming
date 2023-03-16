a55a2e111bf354ce0284e398049afe4d4c290be4
viictoo <provicml@gmail.com>
2023-03-16 11:25:56 +0000

    adv tested and cleaned

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,16 @@
 #include <unistd.h>
 #include <stdio.h>
 
-/*
- * main - A program that prints a string
+/**
+ * Description:main-A program that prints a string
+ *
+ * string
+ *
  * Return: 1(exit status error)
  */
 
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
