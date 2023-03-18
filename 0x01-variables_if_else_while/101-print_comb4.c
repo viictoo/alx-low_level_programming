6ad08cc86352efb3c5f7f63f6636967317207d91
viictoo <provicml@gmail.com>
2023-03-18 06:59:59 +0000

    adv tasks and limits of 8-hexadecimal

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -21,7 +21,7 @@
 		putchar(x + '0');
 		putchar(y + '0');
 		putchar(z + '0');
-		if (x == 7 || y == 8 || z == 9)
+		if (x == 7 && y == 8 && z == 9)
 		continue;
                     putchar(',');
                     putchar(' ');
--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -13,7 +13,7 @@ int main(void)
 
 	for (i = 0; i < 8; i++)
 		putchar(i + '0');
-	for (i = 0; i < 7; i++)
+	for (i = 0; i < 6; i++)
 		putchar(i + 'a');
 	putchar('\n');
 	return (0);
