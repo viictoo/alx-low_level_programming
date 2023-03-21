7270d541dc0aa480d975ac401b4c99fed2884a73
viictoo <125632594+viictoo@users.noreply.github.com>
2023-03-21 23:02:54 +0300

    error: ISO C90

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -11,14 +11,14 @@
 
 	unsigned long long fib1 = 1, fib2 = 2, next_fib, count;
 
-	printf("%llu, %llu", fib1, fib2);
+	printf("%lld, %lld", fib1, fib2);
 
 	for (count = 3; count <= 98; count++)
 	{
 
 	next_fib = fib1 + fib2;
 
-	printf(", %llu", next_fib);
+	printf(", %lld", next_fib);
 
 	fib1 = fib2;
 	fib2 = next_fib;
