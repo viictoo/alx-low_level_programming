9d1b3c105939e7d698b8044885be9dec432d56fc
viictoo <125632594+viictoo@users.noreply.github.com>
2023-03-21 22:46:13 +0300

    Update 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -11,14 +11,14 @@
 
 	long double fib1 = 1, fib2 = 2, next_fib, count;
 
-	printf("%0Lf, %0Lf", fib1, fib2);
+	printf("%g, %g", fib1, fib2);
 
 	for (count = 3; count <= 98; count++)
 	{
 
 	next_fib = fib1 + fib2;
 
-	printf(", %0Lf", next_fib);
+	printf(", %g", next_fib);
 
 	fib1 = fib2;
 	fib2 = next_fib;
