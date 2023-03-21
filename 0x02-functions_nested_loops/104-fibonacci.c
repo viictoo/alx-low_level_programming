ff7349935f75373a1f757da623e0e3e99019dd5b
viictoo <provicml@gmail.com>
2023-03-21 17:58:25 +0000

    changes to 104 - use putchar in main

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,28 +1,26 @@
-#include "main.h"
+#include <unistd.h>
 #include <stdio.h>
 
 /**
- * print_fibonacci - prints the first 98 Fibonacci numbers
+ * main - prints the first 98 Fibonacci numbers
  *
- * @n: lu integer
  * Return: void
  *
- **/
-void print_number(unsigned long int n);
+ */
 
-void print_fibonacci(void)
+int main(void)
 
 {
 	unsigned long int fibb1 = 1, fibbnow = 2, result;
 
 	int i;
 
-	_putchar('1');
-	_putchar(',');
-	_putchar(' ');
-	_putchar('2');
-	_putchar(',');
-	_putchar(' ');
+	putchar('1');
+	putchar(',');
+	putchar(' ');
+	putchar('2');
+	putchar(',');
+	putchar(' ');
 
 
 	for (i = 3; i <= 98; i++)
@@ -32,41 +30,25 @@ void print_fibonacci(void)
 		fibb1 = fibbnow;
 		fibbnow = result;
 
-		if (result > 99999999999999999)
-
-		{
-			fibb1 /= 10;
-			fibbnow /= 10;
-			result /= 10;
-		}
-
-		print_number(result);
 
 		if (i != 98)
 		{
-			_putchar(',');
-			_putchar(' ');
+			putchar(',');
+			putchar(' ');
+		}
+		if (result >= 10)
+		{
+			putchar('0' + (result / 10);
+			putchar('0' + (result % 10));
+
+		}
+		else
+		{
+		putchar(' ');
+		putchar('0' + result);
 		}
 	}
+		putchar('\n');
+		return (0);
 
-	_putchar('\n');
-
-}
-
-/**
- * print_number - prints a number
- *
- * @n: lu integer
- *
- * Return: void
- *
- */
-
-void print_number(unsigned long int n)
-{
-
-	if (n / 10)
-		print_number(n / 10);
-
-	_putchar((n % 10) + '0');
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -28,8 +28,4 @@ void print_to_98(int n);
 
 void print_times_table(int n);
 
-void print_fibonacci(void);
-
-void print_number(unsigned long int n);
-
 #endif 
