0f73ece1a6b318d1dd87daa8b65418ee4f71ba41
viictoo <125632594+viictoo@users.noreply.github.com>
2023-03-21 21:38:59 +0300

    Update 104-fibonacci.c
    final push - putchar now in main -removed functions and main header

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,72 +1,54 @@
-#include "main.h"
+#include <unistd.h>
 #include <stdio.h>
 
 /**
- * print_fibonacci - prints the first 98 Fibonacci numbers
- *
- * @n: lu integer
- * Return: void
- *
- **/
-void print_number(unsigned long int n);
-
-void print_fibonacci(void)
-
-{
-	unsigned long int fibb1 = 1, fibbnow = 2, result;
-
-	int i;
-
-	_putchar('1');
-	_putchar(',');
-	_putchar(' ');
-	_putchar('2');
-	_putchar(',');
-	_putchar(' ');
-
-
-	for (i = 3; i <= 98; i++)
-
-	{
-		result = fibb1 + fibbnow;
-		fibb1 = fibbnow;
-		fibbnow = result;
-
-		if (result > 99999999999999999)
-
-		{
-			fibb1 /= 10;
-			fibbnow /= 10;
-			result /= 10;
-		}
-
-		print_number(result);
-
-		if (i != 98)
-		{
-			_putchar(',');
-			_putchar(' ');
-		}
-	}
-
-	_putchar('\n');
-
-}
-
-/**
- * print_number - prints a number
- *
- * @n: lu integer
+ * main - prints the first 98 Fibonacci numbers
  *
  * Return: void
  *
  */
 
-void print_number(unsigned long int n)
+int main(void)
+
 {
+        unsigned long int fibb1 = 1, fibbnow = 2, result;
 
-	if (n / 10)
-		print_number(n / 10);
+        int i;
 
-	_putchar((n % 10) + '0');
+        putchar('1');
+        putchar(',');
+        putchar(' ');
+        putchar('2');
+        putchar(',');
+        putchar(' ');
+
+
+        for (i = 3; i <= 98; i++)
+
+        {
+                result = fibb1 + fibbnow;
+                fibb1 = fibbnow;
+                fibbnow = result;
+
+
+                if (i != 98)
+                {
+                        putchar(',');
+                        putchar(' ');
+                }
+                if (result >= 10)
+                {
+                        putchar('0' + (result / 10);
+                        putchar('0' + (result % 10));
+
+                }
+                else
+                {
+                putchar(' ');
+                putchar('0' + result);
+                }
+        }
+                putchar('\n');
+                return (0);
+
 }
