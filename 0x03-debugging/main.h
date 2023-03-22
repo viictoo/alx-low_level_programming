9aad2d7c7081c6c8924c43708c1648b38e531396
viictoo <provicml@gmail.com>
2023-03-22 15:43:45 +0000

    Update 0x03 main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,5 +1,5 @@
-#iifndef MAIN_H
-#define MAIN_H
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
 #include <stdio.h>
 
@@ -7,4 +7,4 @@ void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
 void print_remaining_days(int month, int day, int year);
 
-#endif /* MAIN_H */
+#endif /*_MAIN_H_ */
