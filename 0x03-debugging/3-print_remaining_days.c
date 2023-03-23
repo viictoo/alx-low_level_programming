dc4a0a6f33c9052de63af0f880564fb6ac82b7d8
viictoo <provicml@gmail.com>
2023-03-23 19:15:16 +0000

    Fixed 3 print remaining days

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -32,6 +32,6 @@ void print_remaining_days(int month, int day, int year)
 	else
 	{
 		printf("Day of the year: %d\n", day);
-		printf("Remaining days: %d/n", 365 - day);
+		printf("Remaining days: %d\n", 365 - day);
 	}
 	}
