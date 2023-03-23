e7a9b7059bf0d02c7c314bbd3dd7a29f675e1ab2
viictoo <provicml@gmail.com>
2023-03-23 22:22:45 +0000

    Update fizzy

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -17,9 +17,11 @@ int main(void)
 	{
 
 		if (i % 3 == 0)
-			printf("Fizz");
+			printf("Fizz ");
 		if (i % 5 == 0)
-			printf("Buzz");
+			printf("Buzz ");
+		if (i % 5 == 0 && i % 3 == 0)
+			printf("FizzBuzz ");
 	}
 		else
 		{
