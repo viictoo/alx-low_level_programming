c4b75533c94ef57947457ce209a8780f5599a242
viictoo <provicml@gmail.com>
2023-03-30 15:30:34 +0000

    Update 101

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,8 +8,6 @@
 
 void print_number(int n)
 {
-	int i;
-
 	if (n == 0)
 	{
 		_putchar('0');
@@ -18,31 +16,31 @@ void print_number(int n)
 	if (n < 0)
 	{
 		_putchar('-');
-		i = -n;
+		n = -n;
 	}
 	else
-		i = n;
+		n = n;
 
+
+	if (n >= 10)
+	{
+		print_number(n / 10);
+	}
+	_putchar(n % 10 + '0');
 /*
- *	if (n >= 10)
- *	{
- *		print_number(n / 10);
- *	}
- *	_putchar(n % 10 + '0');
-**/
-	int divisor = 1000000000;
-
-	while (i / divisor == 0)
-	{
-		divisor /= 10;
-	}
-
-	while (divisor > 0)
-	{
-	_putchar(i / divisor + '0');
-	i %= divisor;
-	divisor /= 10;
-	}
+*	int divisor = 1000000000;
+*
+*	while (n / divisor == 0)
+*	{
+*		divisor /= 10;
+*	}
+*
+*	while (divisor > 0)
+*	{
+*	_putchar(n / divisor + '0');
+*	n %= divisor;
+*	divisor /= 10;
+*/
 }
 
 /*rework to use loop*/
