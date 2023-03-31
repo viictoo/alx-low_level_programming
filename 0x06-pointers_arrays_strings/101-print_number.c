97c05a97de51694c3894494515ba20b01699a886
viictoo <provicml@gmail.com>
2023-03-31 16:59:37 +0000

    Update var sizes on 9

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,6 +8,8 @@
 
 void print_number(int n)
 {
+	unsigned int a = n;
+
 	if (n == 0)
 	{
 		_putchar('0');
@@ -16,17 +18,17 @@ void print_number(int n)
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		i = -n;
 	}
 	else
-		n = n;
+		i = n;
 
 
-	if (n >= 10)
+	if (i >= 10)
 	{
-		print_number(n / 10);
+		print_number(i / 10);
 	}
-	_putchar(n % 10 + '0');
+	_putchar(i % 10 + '0');
 /*
 *	int divisor = 1000000000;
 *
