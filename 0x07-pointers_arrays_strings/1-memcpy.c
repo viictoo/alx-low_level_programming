b3f93c9ae24ce52ae3211e589fe86521e4847632
viictoo <provicml@gmail.com>
2023-04-03 11:27:38 +0000

    Update 0x07 memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -12,14 +12,11 @@
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
-	char *r = dest;
 
 	for (i = 0; i < n; i++)
 	{
 		dest[i] = src[i];
 	}
 
-	dest[i] = '\0';
-
-	return (r);
+	return (dest);
 }
