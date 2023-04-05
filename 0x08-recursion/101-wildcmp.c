1b4c9e655413bb54d3707e8caa8291a611acdcdc
viictoo <provicml@gmail.com>
2023-04-05 15:28:30 +0000

    101

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -14,18 +14,14 @@ int wildcmp(char *s1, char *s2)
 
 	else if (*s2 == '*')
 	{
-		if (*(s2 + 1) == '\0')
-			return (1);
-		{
-			if (*s1 != '\0')
-			{
-			return (wildcmp(s1, s2 + 1));
+	return ((wildcmp(s1, s2 + 1)) ||
+		(*s1 != '\0' && wildcmp(s1 + 1, s2)));
 
-			}
 	}
-	else if (*s1 == *s2)
+
+	else if (*s1 == *s2 || *s2 == '?')
 		return (wildcmp(s1 + 1, s2 + 1));
-	else
+
 		return (0);
 }
 
