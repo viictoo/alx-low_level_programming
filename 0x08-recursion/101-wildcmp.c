a960cf557280b170b18b9bb31c99b5f9c022ecc1
viictoo <provicml@gmail.com>
2023-04-05 21:07:29 +0000

    Update 101

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -8,14 +8,14 @@
 int clutch(char *s1, char *s2)
 {
 	if (*s1 == '\0' && *s2 == '\0')
-		return (0);
+		return (1);
 	if (*s2 == '*')
 	{
 		if (*s1 == '\0')
 			return (clutch(s1, s2 + 1) || (clutch(s1 + 1, s2)));
 	}
 		return ((*s1 == *s2) && clutch(s1 + 1, s2 + 1));
-
+}
 /**
   * wildcmp-A function that checks if strings can be considered identical
   * @s1: Base String to match
@@ -27,20 +27,20 @@ int wildcmp(char *s1, char *s2)
 {
 	if (*s2 == '\0')
 	{
-		if (s1 == '\0')
+		if (*s1 == '\0')
 			return (1);
 		return (0);
 	}
 
 	else if (*s1 == '\0')
 	{
-	return (*s2 == '*' (wildcmp(s1, s2 + 1)));
+	return (*s2 == '*' && (wildcmp(s1, s2 + 1)));
 	}
 	else if (*s2 == *)
 	{
 		return (clutch(s1, s2));
 
 	}
-	return ((*s1 == s2) && wildcmp(s1 + 1, s2 + 1));
+	return ((*s1 == *s2) && wildcmp(s1 + 1, s2 + 1));
 }
 
