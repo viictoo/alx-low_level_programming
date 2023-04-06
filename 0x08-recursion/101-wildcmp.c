2f8dcdf97fdab3a48652214cacd44aac6c7e010a
viictoo <provicml@gmail.com>
2023-04-06 17:11:00 +0000

    Update 101 indent

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -8,15 +8,19 @@
 int clutch(char *s1, char *s2)
 {
 	if (*s1 == '\0' && *s2 == '\0')
+	{
 		return (1);
+	}
+
 	if (*s2 == '*')
 	{
 		if (*s1 == '\0')
+		{
 			return (clutch(s1, s2 + 1));
-
-			return ((clutch(s1 + 1, s2)) || clutch(s1, s2 + 1));
+		}
+		return ((clutch(s1 + 1, s2)) || clutch(s1, s2 + 1));
 	}
-		return ((*s1 == *s2) && clutch(s1 + 1, s2 + 1));
+	return ((*s1 == *s2) && clutch(s1 + 1, s2 + 1));
 }
 /**
   * wildcmp-A function that checks if strings can be considered identical
