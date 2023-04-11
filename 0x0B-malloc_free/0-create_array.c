4235eb573aaabbc62f5eb66aba6ab5f6c141e651
viictoo <provicml@gmail.com>
2023-04-11 14:36:16 +0000

    corrected 0x0b

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -18,10 +18,10 @@ char *create_array(unsigned int size, char c)
 
 	str = malloc(size * sizeof(char));
 	
-	if (str == null)
+	if (str == NULL)
 		return (NULL);
 
-	for (i = 0, i < size; i++)
+	for (i = 0; i < size; i++)
 		str[i] = c;
 
 	return (str);
