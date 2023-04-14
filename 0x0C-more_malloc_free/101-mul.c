ba5de573d5dfcfbd1b6eddbabcb048266adf9e8f
viictoo <provicml@gmail.com>
2023-04-14 12:02:14 +0000

    mul

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -8,50 +8,39 @@
  * mul- A function that multiplies two numbers
  * @num1: integer
  * @num2: integer
- * @len_1: length of string 1
- * @len_2: length of string 2
+ * @len1: length of string 1
+ * @len2: length of string 2
  * Return: integer product
  */
-
-int mul(char *num1, char *num2, int len_1, int len_2)
+long int mul(long num1, long num2, int len1, int len2)
 {
+	int i, j, n1, n2, carry, swap;
+	long int result = 0, mul1 = 1, mul2 = 1;
 
-	int *result = malloc(sizeof(int) * (len_1 + len_2));
-	int i, j, k, start, carry, sum, n1, n2;
-
-	for (i = 0; i < (len_1 + len_2 - 1); i++)
-		result[i] = 0;
-	i = 0;
-	for (j = (len_1 - 1); j >= 0; j--)
+	for (i = len1 - 1; i >= 0; i--)
 	{
-		carry = 0;
-		n1 = num1[j] - '0';
-		k = 0;
-		for (k = len_2 - 1; k >= 0; k--)
+		n1 = num1 % 10;
+		mul2 = 1;
+
+		for (j = len2 - 1; j >= 0; j--)
 		{
-			n2 = num2[k] - '0';
-			sum = n1 * n2 + result[i + k] + carry;
-			carry = sum / 10;
-			result[i + k] = sum % 10;
+			n2 = num2 % 10;
+			carry = 0;
+
+			swap = n1 * n2 + carry + (result / 10);
+			carry = swap / 10;
+			result = swap % 10;
+			mul2 *= 10;
 		}
+
 		if (carry > 0)
-			result[i + k] += carry;
-		i++;
-	}
-	start = 0;
-	for (i = len_1 + len_2 - 1; i >= 0; i--)
-	{
-		if (result[i] != 0)
-		{
-			start = i;
-			break;
-		}
-	}
-	for (i = start; i >= 0; i--)
-		printf("%d", result[i]);
+			result += carry * mul2;
 
-	printf("\n");
-	free(result);
+		mul1 *= 1;
+		result *= mul1;
+	}
+
+	return (result);
 }
 
 /**
@@ -63,45 +52,42 @@ int mul(char *num1, char *num2, int len_1, int len_2)
 
 int main(int argc, char *argv[])
 {
-	(void) argc;
-	(void) argv[];
-/*
-*	long result;
-*	long num1, num2;
-*	int i;
-*	if (argc != 3)
-*	{
-*		printf("Error\n");
-*		exit(98);
-*	}
-*	for (i = 0; i < strlen(argv[1]); i++)
-*	{
-*		if (!isdigit(argv[1][i]))
-*		{
-*			printf("Error\n");
-*			exit(98);
-*		}}
-*	for (i = 0; i < strlen(argv[1]); i++)
-*	{
-*		if (!isdigit(argv[1][i]))
-*		{
-*			printf("Error\n");
-*			exit(98);
-*		}
-*	}
-*	if (num1 == 0 || num2 == 0)
-*	{
-*		printf("Error\n");
-*		exit(98);
-*	}
-*	int  len_1 = strlen(argv[1]);
-*	int  len_2 = strlen(argv[2]);
-*
-*	num1 = strtol(argv[1], NULL, 10);
-*	num2 = strtol(argv[2], NULL, 10);
-*
-*	result = (num1, num2);
-*	printf("%ld\n", result);
-*/	return (0);
+	long result;
+	long num1, num2;
+	unsigned int i, len_1, len_2;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	for (i = 0; i < strlen(argv[1]); i++)
+	{
+		if (!isdigit(argv[1][i]))
+		{
+			printf("Error\n");
+			exit(98);
+		}}
+	for (i = 0; i < strlen(argv[2]); i++)
+	{
+		if (!isdigit(argv[2][i]))
+		{
+			printf("Error\n");
+			exit(98);
+		}
+	}
+	len_1 = strlen(argv[1]);
+	len_2 = strlen(argv[2]);
+
+	num1 = strtol(argv[1], NULL, 10);
+	num2 = strtol(argv[2], NULL, 10);
+	 if (num1 == 0 || num2 == 0)
+	 {
+		 printf("Error\n");
+		 exit(98);
+	 }
+	result = mul(num1, num2, len_1, len_2);
+	printf("%ld\n", result);
+	return (0);
 }
 
