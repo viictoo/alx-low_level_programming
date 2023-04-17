b57d3bd6442c544729ea9f076899f08658bf2cb2
viictoo <provicml@gmail.com>
2023-04-17 15:03:13 +0000

    Update doh header

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -25,4 +25,3 @@ void print_dog(struct dog *d)
 	else
 		printf("Owner: %s\n", d->owner);
 }
-
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,9 @@
 #ifndef DOG_H
 #define DOG_H
 
+
+int _putchar(char c);
+
 /**
   * struct dog-Contains dog elements - dog name, age, owner
   * @name: dog name type = char *
@@ -22,7 +25,6 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-int _putchar(char c);
 
 
 #endif /* define DOG_H */
