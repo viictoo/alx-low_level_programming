3de41992c91e1add23797e26641e1c808cef7c63
viictoo <provicml@gmail.com>
2023-04-19 16:59:00 +0000

    for loop ver 4

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -22,7 +22,7 @@ int (*get_op_func(char *s))(int, int)
 
 	int i;
 
-	for (; (ops[i].op != NULL); i++)
+	for (i = 0; (ops[i].op != NULL); i++)
 	{
 		if (!(strcmp(s, (ops[i]).op)))
 			return (ops[i].f);
