b6b47284bae5b20324aa01c4ca5fcf9bfab13b1c
viictoo <provicml@gmail.com>
2023-04-19 14:47:15 +0000

    Update 3 limits

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -20,7 +20,7 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
-	if (argv[2][1] != '\0')
+	if (!(get_op_func(argv[2])) || argv[2][1] != '\0')
 	{
 		printf("Error\n");
 		exit(98);
