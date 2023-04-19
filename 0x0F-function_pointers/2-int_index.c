5a1fd0797a78b83a6fa844aad88e8a1133f9f6ce
viictoo <provicml@gmail.com>
2023-04-19 14:04:34 +0000

    Updated sizet

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -13,13 +13,14 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size <= 0 || !cmp)
-		return (-1);
+	if (size > 0 || cmp || array)
+	{
 
 	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]))
 			return (i);
+	}
 	}
 		return (-1);
 }
