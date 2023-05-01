473c0f5366d52e66368263e4620e696ab7c6f16b
viictoo <provicml@gmail.com>
2023-05-01 10:23:06 +0000

    free 5 case NULL

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -11,7 +11,7 @@ void free_listint2(listint_t **head)
 	listint_t *flush, *temp;
 
 	if (!head || !(*head))
-		return (NULL);
+		return;
 
 	for (temp = *head, *head = NULL; temp; temp = temp->next, free(flush))
 		flush = temp;
