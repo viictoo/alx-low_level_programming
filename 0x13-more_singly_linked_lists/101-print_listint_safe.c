29819455d2559515bb2e8818371fe2effbab62aa
viictoo <provicml@gmail.com>
2023-05-02 18:02:35 +0000

    floyds loop

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -7,43 +7,43 @@
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *hare = head, *kobe = head;
-	size_t i = 0, j = 0, flag = 0;
+	const listint_t *kobe = head, *fast = head;
+	size_t count = 0;
 
-	if (head == NULL)
+	if (!head)
 		return (0);
-	while (kobe && hare && hare->next)
+	while (kobe)
+	{
+		if (!fast || !(fast->next))
+			break;
+		printf("[%p] %d\n", (void *)kobe, kobe->n);
+		count++;
+		kobe = kobe->next;
+		fast = fast->next->next;
+
+		if (kobe == fast)
+		{
+			kobe = head;
+			while (kobe)
+			{
+				printf("[%p] %d\n", (void *)fast, fast->n);
+				kobe = kobe->next;
+				fast = fast->next;
+				count++;
+				if (kobe == fast)
+				{
+					printf("-> [%p] %d\n", (void *)fast, fast->n);
+					return (count);
+				}	}	}
+		if (!kobe)
+			exit(98);
+	}
+	while (kobe)
 	{
 		printf("[%p] %d\n", (void *)kobe, kobe->n);
-		i++;
-		hare = hare->next->next;
+		count++;
 		kobe = kobe->next;
-		if (hare == kobe)
-		{
-			flag = 1;
-			break;
-		}	}
-	if (flag)
-	{
-		kobe = head;
-		for (j = 0; j < i - 1; j++)
-		{
-			if (hare == kobe)
-			{
-				printf("-> [%p] %d\n", (void *)hare, hare->n);
-				break;
-			}
-			printf("[%p] %d\n", (void *)hare, hare->n);
-			hare = hare->next;
-			kobe = kobe->next;
-		}
-		i = j + 1;	}
-	else
-	{
-		while (kobe)
-		{
-			printf("[%p] %d\n", (void *)kobe, kobe->n);
-			i++;
-			kobe = kobe->next;
-		}		}
-	return (i);	}
+
+	}
+	return (count);
+}
