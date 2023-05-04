7ad92a0d2a0c038cce7640943dffe42128d2c052
viictoo <provicml@gmail.com>
2023-05-04 17:33:57 +0300

    betty are u ok

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,11 +9,14 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int n = 0;
-	if (!b) return n;
+
+	if (!b)
+		return (n);
 	while (*b)
 	{
-		if (*b != '0' && *b != '1') return 0;
+		if (*b != '0' && *b != '1')
+			return (0);
 		n = (n << 1) + (*b++ - '0');
 	}
-	return n;
+	return (n);
 }
