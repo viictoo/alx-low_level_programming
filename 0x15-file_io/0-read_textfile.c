78e9da6f928eb7fad9862ce70918aeee934ffe83
viictoo <provicml@gmail.com>
2023-05-09 09:57:42 +0300

    Update correct

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,10 +7,9 @@
  * @letters: number of letters it should read and print
  * Return: Actual number of  letters it could read and print otherwise 0
  */
-
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fildes;
+	int fildes, stdout_copy;
 	ssize_t read_count, write_count;
 	char *buffer;
 
@@ -29,22 +28,24 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	{
 		free(buffer);
 		close(fildes);
-		return (0);
-	}
+		return (0);	}
+
+	stdout_copy = dup(STDOUT_FILENO);
 	write_count = write(STDOUT_FILENO, buffer, read_count);
 	if (write_count == -1 || write_count != read_count)
 	{
 		free(buffer);
 		close(fildes);
-		return (0);
-	}
+		return (0);	}
 	write_count = write(STDERR_FILENO, buffer, read_count);
 	if (write_count == -1 || write_count != read_count)
 	{
 		free(buffer);
 		close(fildes);
 		return (0);	}
+
 	free(buffer);
 	close(fildes);
+	close(stdout_copy);
 	return (write_count);
 }
