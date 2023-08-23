fcd0350593f14cefe1bbeea5ea890dccf48c3621
Victor Langat <provicml@gmail.com>
2023-08-23 14:45:29 +0300

    added edge buffer

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -7,11 +7,17 @@ def island_perimeter(grid):
     if len(grid) == 0:
         return
     p = 0
-    for i in range(len(grid) - 1):
-        for j in range(len(grid[i]) - 1):
-        #     print(grid[i][j])
-            if grid[i][j] ^ grid[i + 1][j]:
+    ax, ay = len(grid), len(grid[0])
+    buff_grid = [[0] * (ay + 2) for _ in range(ax + 2)]
+
+    for i in range(ax):
+        for j in range(ay):
+            buff_grid[i + 1][j + 1] = grid[i][j]
+
+    for i in range(len(buff_grid) - 1):
+        for j in range(len(buff_grid[i]) - 1):
+            if buff_grid[i][j] ^ buff_grid[i + 1][j]:
                 p += 1
-            if grid[i][j] ^ grid[i][j + 1]:
+            if buff_grid[i][j] ^ buff_grid[i][j + 1]:
                 p += 1
     return p
