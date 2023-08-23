63b0bc7a4206eebcf09881c569a143bf4dbadc93
Victor Langat <provicml@gmail.com>
2023-08-23 13:58:44 +0300

    init commit all

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''grid perimeter code'''
+
+
+def island_perimeter(grid):
+    '''module to get the perimeter using xor'''
+    if len(grid) == 0:
+        return
+    p = 0
+    for i in range(len(grid)):
+        for j in range(len(grid)):
+            if grid[i][j] ^ grid[i-1][j]:
+                p += 1
+            if grid[i][j] ^ grid[i][j + 1]:
+                p += 1
+    return p
