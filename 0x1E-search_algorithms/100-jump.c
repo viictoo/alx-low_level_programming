b347887f473ed26e663bd0a01173723d02599dfd
Victor Langat <provicml@gmail.com>
2023-10-11 19:14:54 +0300

    test: newtons square method

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -66,7 +66,7 @@ int jump_search(int *array, size_t size, int value)
 	if (!array || size == 0)
 		return (-1);
 
-	jump = (sqrt(size));
+	jump = (squareRoot(size));
 
 	for (i = 0; i < size && array[i] < value; count = i, i += jump)
 	{
